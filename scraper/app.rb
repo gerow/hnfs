5c3e5c3f9bede4411d1b50f03c087973c2d5a795
gerow <gerow.mike@gmail.com>
2013-12-26 22:10:42 -0600

    Scraper is complete as far as fetching the front page is concerned

--- scraper/app.rb
+++ scraper/app.rb
@@ -1,10 +1,27 @@
 require 'sinatra'
 require 'hnscraper'
+require 'json'
+require 'redis'
 
 scraper = HNScraper.new
 redis = Redis.new
 
+crawl_delay = 30
+# hn's robots.txt lists a crawl delay of 30 seconds.
+# Since we might also need to request comments for
+# articles we cache 4x as long. We may ocasionally
+# go over the crawl delay, but this should serve
+# as an ok buffer.
+
 get '/' do
   content_type 'application/json'
-  scraper.front_page
+  content = redis.get('front_page')
+  if not content
+    content = scraper.front_page.to_json
+    redis.set 'front_page', content
+    redis.expire 'front_page', crawl_delay * 4
+  end
+  cache_control :public, :max_age => redis.ttl('front_page')
+  puts "Cache ttl is #{redis.ttl 'front_page'}"
+  content
 end
--- scraper/lib/hnscraper.rb
+++ scraper/lib/hnscraper.rb
@@ -1,29 +1,30 @@
-require 'redis'
 require 'nokogiri'
 require 'open-uri'
-require 'json'
+require 'uri'
 
 class HNScraper
   # This is all super hacky, but really what scraper isn't?
   def initialize params={}
-    @redis = Redis.new
     @url = params[:url] || "https://news.ycombinator.com/"
   end
 
   def front_page
-    if not @redis.get('front_page')
-      doc = Nokogiri::HTML(open(@url))
-      p doc
-      sliced_story_xmls = doc.css("table>tr>td>table>tr").each_slice(3).to_a[0..29]
-      stories = sliced_story_xmls.map{|story_xmls|
-        story = {}
-        story[:url] = story_xmls[1].css("td.title>a").attr("href").value
-        story[:title] = story_xmls[1].css("td.title>a").text
-        story
-      }
-      @redis.set('front_page', stories.to_json)
-      @redis.expire('front_page', 10)
-    end
-    return @redis.get('front_page')
+    doc = Nokogiri::HTML(open(@url))
+    p doc
+    sliced_story_xmls = doc.css("table>tr>td>table>tr").each_slice(3).to_a[0..29]
+    stories = sliced_story_xmls.map{|story_xmls|
+      story = {}
+      story[:url] = story_xmls[1].css("td.title>a").attr("href").value
+      story_uri = URI(story[:url])
+      if not story_uri.host
+        story[:url] = @url + story[:url]
+      end
+      story[:title] = story_xmls[1].css("td.title>a").text
+      story[:user] = story_xmls[2].css("td.subtext>a")[0].text
+      story[:comments] = story_xmls[2].css("td.subtext>a")[1].attr("href")
+      story[:comments] = @url + story[:comments]
+      story
+    }
+    return stories
   end
 end
